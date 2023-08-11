9b6d6d822e88b9d654eeb4ae51377af7acfe1da7
Abhishek Vishnoi <abvishno@redhat.com>
2023-08-11 12:48:12 +0530

    first commit

--- src/main/java/com/example/sbcamelkafka/CopsSchedulerRoute.java
+++ src/main/java/com/example/sbcamelkafka/CopsSchedulerRoute.java
@@ -5,17 +5,27 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SchedulerRoute extends RouteBuilder {
+public class CopsSchedulerRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
 
+        /**
+         * Rest configuration for the Rest API .
+         */
         restConfiguration()
                 .enableCORS(true)
                 .component("jetty")
                 .host("0.0.0.0")
                 .port(8084);
 
+        /**
+         * A Rest Endpoint Route to Trigger the COPS .
+         * The rest endpoint is for a fallback mechanism
+         * which can be used to trigger report for previously
+         * failed scenarios . The Rest endpoint will be
+         * triggered form a web Application.
+         */
         rest("/api")
                 .get("/rerunFlightReportGeneration")
                 .to("direct:rerunFlightReportGeneration");
@@ -25,15 +35,24 @@ public class SchedulerRoute extends RouteBuilder {
                 .log("Rerunning an Adhoc request for report Generation.")
                 .to("direct:startReportGeneration");
 
+        /**
+         * A Scheduler Route to Trigger the COPS .
+         * The scheduler will eventually be removed after Indigo Starts
+         * using the Kubernetes pod Scheduler .
+         */
         from("quartz://timerName?cron={{expression}}")
                 .routeId("freshFlightReportGeneration")
-                .log("Starting a frest report generation .")
+                .log("Starting a fresh report generation .")
                 .to("direct:startReportGeneration");
 
+        /**
+         * A route for report generation , both Rest Request
+         * and the Scheduler converges to this route
+         */
         from("direct:startReportGeneration")
                 .routeId("startReportGeneration")
                 .log("Fetch Information from  Navitaire .")
-                .setBody(simple("{hurray}"))
+                .to("http://{{flight.data.api.host}}:{{flight.data.api.port}}/{{flight.data.api.path}}?bridgeEndpoint=true")
                 .log("Sending data to Kafka Topic")
                 .to("kafka:{{topic}}?brokers={{broker}}");
 
